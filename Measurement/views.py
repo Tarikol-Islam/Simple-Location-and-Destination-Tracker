984c81e29f69eda5eae2c6673331f2faf1ac26e2
Mohammad Tarikol Islam <tuhidtitw98@gmail.com>
2021-09-16 19:26:39 +0600

    "Destination updated"

--- Measurement/views.py
+++ Measurement/views.py
@@ -5,21 +5,22 @@ from .models import Measurement
 from .forms import MeasurementModelForm
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
-from .utils import get_geo , get_center_coordinates, get_zoom
+from .utils import get_geo , get_center_coordinates, get_ip_address, get_zoom
 import folium
 # Create your views here.
 def calculate_distance_view(request):
+    Distance = None
     obj = get_object_or_404(Measurement, id = 1)
     form = MeasurementModelForm(request.POST or None)
     geolocator = Nominatim(user_agent='Measurement')
-    ip = '103.230.106.50'
+    ip = get_ip_address(request)
     S_country , S_city, S_lat ,S_lon = get_geo(ip)
     StartingPoint = (S_lat, S_lon)
 
     #Folium map part
     M = folium.Map(width=700,height=500, location = get_center_coordinates(S_lat,S_lon), zoom_start = 8)
     #Location marker
-    folium.Marker([S_lat,S_lon], tooltip = "Click Here for more",popup=S_city['city'], icon= folium.Icon(color = "purple")).add_to(M)
+    folium.Marker([S_lat,S_lon], tooltip = "Click Here for more",popup=S_city['city'], icon= folium.Icon(color = "red")).add_to(M)
 
     if form.is_valid():
         instance = form.save(commit=False)
@@ -30,16 +31,18 @@ def calculate_distance_view(request):
         #Folium map part
         M = folium.Map(width=700,height=500,location = get_center_coordinates(*(StartingPoint+EndingPoint)), zoom_start =get_zoom(Distance))
         #Location marker
-        folium.Marker([S_lat,S_lon], tooltip = "Click Here for more",popup=S_city['city'], icon= folium.Icon(color = "purple")).add_to(M)
+        folium.Marker([S_lat,S_lon], tooltip = "Click Here for more",popup=S_city['city'], icon= folium.Icon(color = "red")).add_to(M)
         #Destination Marker
-        folium.Marker(EndingPoint, tooltip = "Click Here for more",popup=destination, icon= folium.Icon(color = "purple")).add_to(M)
-        
+        folium.Marker(EndingPoint, tooltip = "Click Here for more",popup=destination, icon= folium.Icon(color = "green")).add_to(M)
+        line = folium.PolyLine(locations =[StartingPoint,EndingPoint] , weight=1, color = 'blue')
+        M.add_child(line)
         instance.location = geolocator.geocode(S_city)
         instance.distance = Distance
         instance.save()
     M = M._repr_html_()
     context= {
-        'distance': obj,
+        'distance': Distance,
+        'destination':destination,
         'form' : form,
         'map' : M,
     }
